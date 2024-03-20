14ccc4b446dbaf550ce6623bd2931058ac48df5e
DJX <canmeng10964@163.com>
2024-03-20 14:28:24 +0800

    first commit

---
+++ ModuleB/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ModuleB
+//
+//  Created by DJX on 2024/3/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
